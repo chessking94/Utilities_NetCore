1e44f10d707a08f89b7d050ae3cd4e105367c0f8
Ethan Hunt <chessking94@gmail.com>
2024-07-13 21:03:12 -0500

    File

---
+++ Utilities_NetCore/modules/modCommandLine.vb
@@ -0,0 +1,5 @@
+﻿Public Module modCommandLine
+    Public Function ParseCommandLineArguments(args As String()) As Dictionary(Of String, String)
+        Return New Dictionary(Of String, String)
+    End Function
+End Module
