81bf5074f707ca1e913c8ce4625fb176fee44be5
Ethan Hunt <chessking94@gmail.com>
2024-07-14 14:02:43 -0500

    Functional initial iteration

--- Utilities_NetCore/modules/modCommandLine.vb
+++ Utilities_NetCore/modules/modCommandLine.vb
@@ -1,5 +1,29 @@
 ﻿Public Module modCommandLine
     Public Function ParseCommandLineArguments(args As String()) As Dictionary(Of String, String)
-        Return New Dictionary(Of String, String)
+        Dim parameters As New Dictionary(Of String, String)
+        Dim currentKey As String = Nothing
+
+        For Each arg As String In args
+            If arg.StartsWith("-"c) Then
+                'found a hyphen, this is a key. Technically this doesn't allow an actual argument to start with a "-", but oh well
+                'TODO: this also does not support -tTEST style there has to be a space between the positional name and the argument itself
+                currentKey = arg.TrimStart("-"c)
+                If parameters.ContainsKey(currentKey) Then
+                    Throw New Exception($"Found multiple instances of argument '{currentKey}'")
+                Else
+                    parameters(currentKey) = Nothing  'set default value
+                End If
+            Else
+                'no hyphen, it is the value for the most recent key found
+                If currentKey Is Nothing Then
+                    Throw New Exception($"Argument '{arg}' passed with no positional identifier")
+                Else
+                    parameters(currentKey) = arg
+                    currentKey = Nothing
+                End If
+            End If
+        Next
+
+        Return parameters
     End Function
 End Module
