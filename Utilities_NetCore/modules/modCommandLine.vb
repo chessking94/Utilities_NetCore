e1815397de8a88d2402913b29329f421b56076b9
Ethan Hunt <chessking94@gmail.com>
2024-11-24 21:40:47 -0600

    Add RunCommand function

--- Utilities_NetCore/modules/modCommandLine.vb
+++ Utilities_NetCore/modules/modCommandLine.vb
@@ -1,4 +1,6 @@
-﻿Public Module modCommandLine
+﻿Imports System.IO
+
+Public Module modCommandLine
     Public Function ParseCommandLineArguments(args As String()) As Dictionary(Of String, String)
         Dim parameters As New Dictionary(Of String, String)
         Dim currentKey As String = Nothing
@@ -25,5 +27,30 @@
         Next
 
         Return parameters
+    End Function
+
+    Public Function RunCommand(pi_command As String, Optional pi_workingDir As String = Nothing, Optional pi_arguments As String = Nothing, Optional pi_permanent As Boolean = False) As Integer
+        'based on https://stackoverflow.com/a/10263144
+        Dim process As Process = New Process()
+        Dim processInfo As ProcessStartInfo = New ProcessStartInfo()
+        processInfo.CreateNoWindow = True
+
+        If Not Directory.Exists(pi_workingDir) Then
+            Throw New DirectoryNotFoundException
+        End If
+
+        If Not String.IsNullOrWhiteSpace(pi_workingDir) Then processInfo.WorkingDirectory = pi_workingDir
+        processInfo.Arguments = " " + If(pi_permanent = True, "/K", "/C") + " " + pi_command
+
+        If Not String.IsNullOrWhiteSpace(pi_arguments) Then
+            processInfo.Arguments += " "
+            processInfo.Arguments += pi_arguments
+        End If
+        processInfo.FileName = "cmd.exe"
+        process.StartInfo = processInfo
+        process.Start()
+        process.WaitForExit()
+
+        Return process.ExitCode
     End Function
 End Module
