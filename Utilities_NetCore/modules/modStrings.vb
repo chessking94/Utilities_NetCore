3a9d9ac60e7c2642a051aa6b647f0eb820b0e44f
Ethan Hunt <chessking94@gmail.com>
2024-11-29 13:28:26 -0600

    Add AppendText function

--- Utilities_NetCore/modules/modStrings.vb
+++ Utilities_NetCore/modules/modStrings.vb
@@ -1,4 +1,12 @@
 ﻿Public Module modStrings
+    ''' <summary>
+    ''' Remove all occurrences of a list of characters from a string
+    ''' </summary>
+    ''' <param name="pi_Input"></param>
+    ''' <param name="pi_RemoveCharacters"></param>
+    ''' <returns>
+    ''' String without characters present in pi_RemoveCharacters
+    ''' </returns>
     Public Function RemoveCharacters(pi_Input As String, pi_RemoveCharacters As List(Of Char)) As String
         Dim result As String = ""
         For Each ch As Char In pi_Input
@@ -7,5 +15,28 @@
             End If
         Next
         Return result
+    End Function
+
+    ''' <summary>
+    ''' Combine two strings and separate them by a provided delimiter
+    ''' </summary>
+    ''' <param name="pi_Input"></param>
+    ''' <param name="pi_Append"></param>
+    ''' <param name="pi_Delimiter"></param>
+    ''' <returns>
+    ''' Merged strings separated by the provided delimiter
+    ''' </returns>
+    Public Function AppendText(pi_Input As String, pi_Append As String, Optional pi_Delimiter As String = vbCrLf) As String
+        If String.IsNullOrWhiteSpace(pi_Input) Then
+            Return pi_Append
+        Else
+            If pi_Append = "" Then
+                Return pi_Input
+            Else
+                pi_Input += pi_Delimiter
+                pi_Input += pi_Append
+                Return pi_Input
+            End If
+        End If
     End Function
 End Module
